e80146ec703c96c584721c721ea7d10e8da28531
Mark Nakayama <mark.nakay@gmail.com>
2022-11-27 13:19:24 -0700

    styled all_products.html

--- server.py
+++ server.py
@@ -152,6 +152,11 @@ def signout():
     return redirect("/")
 
 
+@app.route("/under_construct")
+def under():
+    
+    return render_template("under_construct.html")
+
 if __name__ == "__main__":
     connect_to_db(app)
     app.run(host="0.0.0.0", debug=True)
